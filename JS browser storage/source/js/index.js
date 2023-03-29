465766f5eb284b6b431f1926eb3f19df52881b0b
Kanan <118839498+kananra@users.noreply.github.com>
2023-03-29 07:17:10 -0800

    update

--- JS browser storage/source/js/index.js
+++ JS browser storage/source/js/index.js
@@ -11,30 +11,30 @@ function addToCart(event) {
   const about = event.target.parentNode.parentNode.children[1].textContent; 
   const price = event.target.parentNode.children[0].textContent;
   
-  // Sepet öğesini oluştur
+
   const cartItem = {
     product: product,
     about: about,
     price: price
   };
   
-  // Local Storage'da önceki sepet öğelerini al
+
   let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
   
-  // Eğer sepet öğesi zaten varsa, güncelle
+
   const existingItemIndex = cartItems.findIndex(item => item.product === product && item.about === about && item.price === price);
   if (existingItemIndex > -1) {
     cartItems[existingItemIndex].quantity += 1;
   } else {
-    // Yeni sepet öğesini ekle
+
     cartItem.quantity = 1;
     cartItems.push(cartItem);
   }
   
-  // Local Storage'da sepet öğelerini güncelle
+
   localStorage.setItem('cartItems', JSON.stringify(cartItems));
   
-  // Sepete görsel olarak öğe ekle
+
   const cartList = document.getElementById('cart-items');
   cartList.innerHTML = '';
   cartItems.forEach(item => {
